dcc7bac389ab2a36f14ddeb620f2afff7d245a5e
Benjamin Buch <benni.buch@gmail.com>
2018-06-12 14:28:56 +0200

    output char types is int

--- include/bitmap/pixel_output.hpp
+++ include/bitmap/pixel_output.hpp
@@ -19,18 +19,51 @@ namespace bmp{ namespace pixel{
 
 	template < typename T >
 	std::ostream& operator<<(std::ostream& os, basic_ga< T > const& v){
-		return os << '{' << v.g << ", " << v.a << '}';
+		if constexpr(
+			std::is_same_v< T, char > ||
+			std::is_same_v< T, signed char > ||
+			std::is_same_v< T, unsigned char >
+		){
+			return os << '{'
+				<< static_cast< int >(v.g) << ", "
+				<< static_cast< int >(v.a) << '}';
+		}else{
+			return os << '{' << v.g << ", " << v.a << '}';
+		}
 	}
 
 	template < typename T >
 	std::ostream& operator<<(std::ostream& os, basic_rgb< T > const& v){
-		return os << '{' << v.r << ", " << v.g << ", " << v.b << '}';
+		if constexpr(
+			std::is_same_v< T, char > ||
+			std::is_same_v< T, signed char > ||
+			std::is_same_v< T, unsigned char >
+		){
+			return os << '{'
+				<< static_cast< int >(v.r) << ", "
+				<< static_cast< int >(v.g) << ", "
+				<< static_cast< int >(v.b) << '}';
+		}else{
+			return os << '{' << v.r << ", " << v.g << ", " << v.b << '}';
+		}
 	}
 
 	template < typename T >
 	std::ostream& operator<<(std::ostream& os, basic_rgba< T > const& v){
-		return os << '{' << v.r << ", " << v.g << ", " << v.b << ", "
-			<< v.a << '}';
+		if constexpr(
+			std::is_same_v< T, char > ||
+			std::is_same_v< T, signed char > ||
+			std::is_same_v< T, unsigned char >
+		){
+			return os << '{'
+				<< static_cast< int >(v.r) << ", "
+				<< static_cast< int >(v.g) << ", "
+				<< static_cast< int >(v.b) << ", "
+				<< static_cast< int >(v.a) << '}';
+		}else{
+			return os << '{' << v.r << ", " << v.g << ", " << v.b << ", "
+				<< v.a << '}';
+		}
 	}
 
 
