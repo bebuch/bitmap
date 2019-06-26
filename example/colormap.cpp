8541c656e9dd08f5083e2e0859465e6d831114ef
Benjamin Buch <benjamin.buch@tu-ilmenau.de>
2019-06-26 11:06:17 +0200

    started colormap implementation

---
+++ example/colormap.cpp
@@ -0,0 +1,32 @@
+#include <bitmap/bitmap.hpp>
+#include <bitmap/colormap.hpp>
+#include <bitmap/binary_write.hpp>
+
+#include <iostream>
+#include <iomanip>
+
+
+int main(){
+	int bits = 8;
+	auto colormap = bmp::colormap::make_uniform({
+		{0   , 0   , 0   , 0   , 1   , 1   , 0.75, 1   },
+		{0   , 0   , 1   , 0.5 , 1   , 0   , 0   , 1   },
+		{0   , 1   , 1   , 0   , 0   , 0   , 1   , 1   }}, bits);
+
+	for(auto v: colormap){
+		std::cout
+			<< std::setw(10) << static_cast< double >(v.r) << " "
+			<< std::setw(10) << static_cast< double >(v.g) << " "
+			<< std::setw(10) << static_cast< double >(v.b) << "\n";
+	}
+
+	bmp::bitmap< bmp::pixel::rgb8u > colorbar(std::pow(2, bits), 10);
+
+	for(std::size_t x = 0; x < colorbar.width(); ++x){
+		for(std::size_t y = 0; y < colorbar.height(); ++y){
+			colorbar(x, y) = colormap[x];
+		}
+	}
+
+	bmp::binary_write(colorbar, "colorbar.bbf");
+}
