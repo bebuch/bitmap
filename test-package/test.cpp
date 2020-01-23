1d4ea57f2750c6bda00d140af7be141e8c895a89
Benjamin Buch <benjamin.buch@tu-ilmenau.de>
2020-01-23 11:42:48 +0100

    add test-package dir

---
+++ test-package/test.cpp
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2020 Benjamin Buch
+//
+// https://gitlab.tu-ilmenau.de/FakMB/QBV/topics/cpp/bitmap
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#include <bitmap/bitmap.hpp>
+
+int main(){
+    bmp::bitmap<float> x;
+    (void)x;
+}
