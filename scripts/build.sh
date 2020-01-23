595d60e89723b55fb481c60bf6bdd5065a0f0bff
Benjamin Buch <benjamin.buch@tu-ilmenau.de>
2020-01-23 11:39:44 +0100

    make library installable via CMake

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,17 +4,31 @@
 set -e
 set -o xtrace
 
-# create build dir
-mkdir build
-cd build
+PROJECT_DIR=$(pwd)
+INSTALL_PATH=$PROJECT_DIR/../tmp/usr
+mkdir -p $INSTALL_PATH
 
-# build
-cmake ..
-make
+# Configure Project
+mkdir -p $PROJECT_DIR/build
+cd $PROJECT_DIR/build
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DBITMAP_BUILD_EXAMPLES=ON -DBITMAP_BUILD_TESTS=ON ..
 
-# unit tests
-test/bitmap_lib_tests
+# Build test
+make -j 32
 
-# clean up
-cd ..
-rm -r build
+# Run tests
+test/tests
+
+# Install
+make install
+
+# Check install
+git diff --no-index $INSTALL_PATH/include/bitmap $PROJECT_DIR/include/bitmap
+test -f $INSTALL_PATH/lib/cmake/bitmap/bitmap-config.cmake
+
+# Check install by example project
+mkdir -p $PROJECT_DIR/build-package-test
+cd $PROJECT_DIR/build-package-test
+cmake -DCMAKE_PREFIX_PATH=$INSTALL_PATH ../test-package
+make -j 32
+./test_bitmap_package
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+set -e        # exit script on any error
+set -o xtrace # print commands in log
 
-# exit script on any error
-set -e
-set -o xtrace
-
+# add temporary credentials and check out the submodules
+printf "machine gitlab.tu-ilmenau.de\nlogin gitlab-ci-token\npassword $CI_JOB_TOKEN\n" > ~/.netrc
 git submodule sync --recursive
 git submodule update --init --recursive
+
+# build and install dependencies
+PROJECT_DIR=$(pwd)
+
+mkdir -p $PROJECT_DIR/3rdparty/io_tools/build
+cd $PROJECT_DIR/3rdparty/io_tools/build
+cmake ..
+make -j 32
+make install
+
+mkdir -p $PROJECT_DIR/3rdparty/colormap/build
+cd $PROJECT_DIR/3rdparty/colormap/build
+cmake ..
+make -j 32
+make install
